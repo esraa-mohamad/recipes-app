c1aa06b4c76c47d5e4228c89f1ea31f75c96a0d4
esdawood <esoodawood186@gmail.com>
2024-11-05 12:47:46 +0200

    - add firebase to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:recipes_book_app/recipes_book_app.dart';
 
 import 'core/routes/app_routing.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp( RecipesBookApp(appRouting: AppRouting(),),);
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    RecipesBookApp(
+      appRouting: AppRouting(),
+    ),
+  );
 }
