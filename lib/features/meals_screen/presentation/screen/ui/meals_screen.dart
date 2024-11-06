16fdd6436dbeaa3de52bc7921cc08d301a871536
fatmaatef304 <fatimaatef990@gmailcom>
2024-11-06 21:37:54 +0200

    add back button

--- lib/features/meals_screen/presentation/screen/ui/meals_screen.dart
+++ lib/features/meals_screen/presentation/screen/ui/meals_screen.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:recipes_book_app/core/theme/app_color.dart';
 import 'package:recipes_book_app/features/meals_screen/presentation/screen/widgets/meals_body.dart';
- 
+
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key});
-   @override
+  @override
   Widget build(BuildContext context) {
-         return Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              title: Text(
-                'Dessert',
-                style: TextStyle(color: Colors.white),
-              ),
-              backgroundColor: AppColor.mainOrange,
-            ),
-            body: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: MealsBody() ,),
-          );
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_circle_left_outlined,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        title: Text(
+          'Meals',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: AppColor.mainOrange,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: MealsBody(),
+      ),
+    );
   }
-   
 }
