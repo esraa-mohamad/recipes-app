2d576fd013af7edf7a32e03b0a2219f77af580b3
esdawood <esoodawood186@gmail.com>
2024-11-07 16:17:26 +0200

    - show name from login in header section in home

--- lib/features/home/presentation/screen/widgets/header_section.dart
+++ lib/features/home/presentation/screen/widgets/header_section.dart
@@ -2,21 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:recipes_book_app/core/helper/app_images.dart';
 import 'package:recipes_book_app/core/theme/app_text_style.dart';
 
+import '../../../../../core/di/dependency_injection.dart';
+import '../../../../../core/shared_pref/app_prefs.dart';
+
 class HeaderSection extends StatelessWidget {
-  const HeaderSection({
+   HeaderSection({
     super.key,
   });
+  final AppPreferences appPreferences = getIt<AppPreferences>();
 
   @override
   Widget build(BuildContext context) {
+    final nameUser = appPreferences.getLoginName();
     return ListTile(
-      leading: Image.asset(AppImages.profile),
+      leading: Image.asset(AppImages.splashLogo),
       title: Text(
         'Welcome, 👋🏻',
         style: AppTextStyle.font16SlateGrayRegular,
       ),
       subtitle: Text(
-        'Fatma Atef',
+        nameUser,
         style: AppTextStyle.font16DarkGreenSemiBold,
       ),
       trailing: Image.asset(AppImages.notification),
